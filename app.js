82c4aada63947ea7311f1f5ce0a0dbedceb10ed1
Adam Bolton <adam_bolton90@hotmail.com>
2022-02-04 22:03:27 +0900

    added mobile menu

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const menu = document.querySelector('.fa-bars');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 function menuToggle() {
     menu.classList.toggle("fa-times");
+    mobileMenu.classList.toggle("show-mobile-menu");
 }
 
 menu.addEventListener('click', menuToggle);
